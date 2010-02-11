6d3eaaa27f706c3b7ca087560750dc7452416b39
ripleyrm <ripleyrm@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-02-11 15:14:57 +0000

    New silent option for siena07, test output altered
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rsiena@55 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/RSienaTest/tests/parallel.R
+++ pkg/RSienaTest/tests/parallel.R
@@ -8,13 +8,17 @@ myeff<- getEffects(mydata)
 mymodel<- model.create(findiff=TRUE, fn = simstats0c, projname='test3',
                        cond=FALSE, nsub=2, n3=100)
 print('test3')
-ans<- siena07(mymodel, data=mydata, effects=myeff,  batch=TRUE, parallelTesting=TRUE, verbose=TRUE)#,dll='../siena/src/RSiena.dll')
+ans<- siena07(mymodel, data=mydata, effects=myeff,  batch=TRUE, parallelTesting=TRUE, silent=TRUE)#,dll='../siena/src/RSiena.dll')
+ans
 ##test4
 mymodel$projname <- 'test4'
 mymodel$cconditional <- TRUE
 mymodel$condvarno<- 1
 print('test4')
-ans<- siena07(mymodel, data=mydata, effects=myeff,  batch=TRUE, parallelTesting=TRUE, verbose=TRUE)#,dll='../siena/src/RSiena.dll')
+ans<- siena07(mymodel, data=mydata, effects=myeff,  batch=TRUE,
+              parallelTesting=TRUE, silent=TRUE)
+##, verbose=TRUE)#,dll='../siena/src/RSiena.dll')
+ans
 ##test7
 mynet1 <- sienaNet(array(c(tmp3,tmp4),dim=c(32,32,2)))
 mydata <- sienaDataCreate(mynet1)
@@ -22,13 +26,19 @@ myeff<- getEffects(mydata)
 mymodel<- model.create(fn = simstats0c, projname='test7', nsub=2, n3=100,
                        cond=FALSE)
 print('test7')
-ans<- siena07(mymodel, data=mydata, effects=myeff,  batch=TRUE, parallelTesting=TRUE, verbose=TRUE)#,dll='../siena/src/RSiena.dll')
+ans<- siena07(mymodel, data=mydata, effects=myeff,  batch=TRUE,
+              parallelTesting=TRUE, silent=TRUE)
+##, verbose=TRUE)#,dll='../siena/src/RSiena.dll')
+ans
 ##test8
 mymodel$projname <- 'test8'
 mymodel$cconditional <- TRUE
 mymodel$condvarno<- 1
 print('test8')
-ans<- siena07(mymodel, data=mydata, effects=myeff,  batch=TRUE, parallelTesting=TRUE, verbose=TRUE)#,dll='../siena/src/RSiena.dll')
+ans <- siena07(mymodel, data=mydata, effects=myeff,  batch=TRUE,
+              parallelTesting=TRUE, silent=TRUE)
+##, verbose=TRUE)#,dll='../siena/src/RSiena.dll')
+ans
 ##test9
 
 mynet1 <- sienaNet(array(c(s501, s502, s503), dim=c(50, 50, 3)))
@@ -41,7 +51,10 @@ print('test10')
 mymodel$projname <- 'test10'
 mymodel$cconditional <- TRUE
 mymodel$condvarno<- 1
-ans<- siena07(mymodel, data=mydata, effects=myeff,  batch=TRUE, parallelTesting=TRUE, verbose=TRUE)
+ans <- siena07(mymodel, data=mydata, effects=myeff, batch=TRUE,
+               parallelTesting=TRUE, silent=TRUE)
+##, verbose=TRUE)
+ans
 ##test11
 print('test11')
 data501 <- sienaDataCreateFromSession("s50.csv", modelName="s50")
@@ -50,5 +63,7 @@ data501paj <- sienaDataCreateFromSession("s50paj.csv", modelName="s50paj")
 
 model501e <- model.create( projname="s50e", cond=FALSE, nsub=2, n3=100 )
 ans501e <- siena07(model501e, data=data501e$mydata, effects=data501e$myeff,
-                   parallelTesting=TRUE, batch=TRUE, verbose=TRUE)
+                   parallelTesting=TRUE, batch=TRUE, silent=TRUE)
+##, verbose=TRUE)
+ans501e
 ## compare with outputs in parallelchecked/
